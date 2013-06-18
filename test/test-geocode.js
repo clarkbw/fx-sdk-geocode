f91733a7b1f58fb4259735415ea59e17fdccca09
Bryan Clark <clarkbw@gmail.com>
2013-06-18 14:20:44 -0700

    add some data caching so we can lookup address types multiple times without worrying that we'll loop through all the data everytime

--- test/test-geocode.js
+++ test/test-geocode.js
@@ -18,7 +18,7 @@ exports["test 001 manual lookup failed promise"] = function(assert, done) {
 };
 
 exports["test 01 get geocode results"] = function(assert, done) {
-	Geocode.on('geocode', function(results) {
+	Geocode.once('geocode', function(results) {
 		assert.pass("got geocoded event");
 		done();
 	});
@@ -32,18 +32,41 @@ exports["test 01 get geocode results"] = function(assert, done) {
 exports["test get geocode results"] = function(assert) {
 	var postal_code = Geocode.getAddressByType('postal_code');
 	assert.ok(postal_code, 'got an address ' + postal_code.formatted_address);
+	// test caching
+	var cache = Geocode.getAddressByType('postal_code');
+	assert.ok(cache, 'got an address from the cache ' + cache.formatted_address);
+	assert.equal(postal_code, cache, 'cache is the same as the original');
 };
 
 exports["test get multiple geocode results"] = function(assert) {
 	var political = Geocode.getAddressesByType('political');
 	assert.ok(Array.isArray(political), 'got multiple');
 	assert.ok(political.length > 0, 'got an address ' + political[0].formatted_address);
+	// check that the cached delivers the same results
+	var cache = Geocode.getAddressesByType('political');
+	assert.ok(Array.isArray(cache), 'got multiple');
+	assert.ok(cache.length > 0, 'got an address from the cache ' + cache[0].formatted_address);
+	assert.equal(political, cache, 'cache is the same as the original');
 };
 
 exports["test manual lookup promise"] = function(assert, done) {
 	Geocode.lookup(Geolocation.coords).then(function success(results) {
 		assert.pass('got some results');
 		done();
+	}, assert.fail);
+};
+
+// we want this one to run last since it blows away the local coords cache
+exports["test zzzz cache removal"] = function(assert, done) {
+	// lookup Vancouver
+	Geocode.lookup({latitude : '49.25', longitude : '-123.1'}).then(function success(results1) {
+		assert.pass('got some results');
+		var postal_code = Geocode.getAddressByType('postal_code');
+		// lookup Boston
+		Geocode.lookup({latitude : '42.358056', longitude : '-71.063611'}).then(function success(results2) {
+			assert.notEqual(postal_code, Geocode.getAddressByType('postal_code'));
+			done();
+		}, assert.fail);
 	}, assert.fail);
 };
 
